761f2fbc29467e328505651b4ccc20f3aa753a62
Josh Haimson <jdhaimson@gmail.com>
2013-02-06 13:34:08 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the home of the code behind my personal website/blog/whatever else I plan on doing with it.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joshhaimson::Application.config.secret_token = 'df0c0d61d8adf6b8568a3d914e0caf7f806ac9474ffe5d19759cd1019f2b65dae5455fabcc26b5d59fe10c5760607f7449f20fd717cf3b7d5dead5f9efa50bcb'
