659cf26849394443f56e1a27e55d34413d1417d4
Josh haimson <jdhaimson@gmail.com>
2013-09-15 22:52:26 -0400

    Get rid of showoffy stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Joshhaimson::Application.routes.draw do
   root to: "static_pages#home"
   match '/mom' => "static_pages#mom"
   match '/bunko' => "static_pages#bunko"
+  match '/showoff' => "static_pages#showoff"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
